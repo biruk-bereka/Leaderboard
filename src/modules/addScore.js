eb00af106b4ede988b2d5378d98ac05eda64644d
biruk-bereka <birukbeletech1212@gmail.com>
2023-01-31 20:57:41 +0300

    modules added

---
+++ src/modules/addScore.js
@@ -0,0 +1,14 @@
+import ScoreList from './scoreList.js';
+
+const list = new ScoreList();
+
+const addScore = (name, score) => {
+  const newScore = {
+    name,
+    score,
+  };
+
+  list.setList(newScore);
+};
+
+export default addScore;
